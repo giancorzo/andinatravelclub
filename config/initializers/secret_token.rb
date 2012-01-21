cf24c0f74f6980ed89766b4604e8a8b01173aac0
Giancarlo Corzo <gian.corzo@antartec.com>
2012-01-21 13:50:44 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndinaSite::Application.config.secret_token = 'f29b95e0901d8722e24da79c44553555e6447094e775056a762f7b26580ca370f73161dfb14d3007d19534bd8ddd051af0f7dd9ccdf54b38b51384f7a111557b'
